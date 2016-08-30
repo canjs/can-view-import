3b9afbb3b5ac8ff01a570906943830a4d385a4d3
Matthew Phillips <matthew@matthewphillips.info>
2016-08-30 10:08:10 -0400

    3.0.0-pre.4

---
+++ dist/global/can-view-import.js
@@ -0,0 +1,153 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-view-import@3.0.0-pre.3#can-view-import*/
+define('can-view-import', function (require, exports, module) {
+    var assign = require('can-util/js/assign/assign');
+    var canData = require('can-util/dom/data/data');
+    var importer = require('can-util/js/import/import');
+    var nodeLists = require('can-view-nodelist');
+    var tag = require('can-view-callbacks').tag;
+    var events = require('can-event');
+    tag('can-import', function (el, tagData) {
+        var moduleName = el.getAttribute('from');
+        var templateModule = tagData.options.get('helpers.module');
+        var parentName = templateModule ? templateModule.id : undefined;
+        if (!moduleName) {
+            return Promise.reject('No module name provided');
+        }
+        var importPromise = importer(moduleName, parentName);
+        canData.set.call(el, 'viewModel', importPromise);
+        canData.set.call(el, 'scope', importPromise);
+        var scope = tagData.scope.add(importPromise);
+        var handOffTag = el.getAttribute('can-tag');
+        if (handOffTag) {
+            var callback = tag(handOffTag);
+            canData.set.call(el, 'preventDataBindings', true);
+            callback(el, assign(tagData, { scope: scope }));
+            canData.set.call(el, 'preventDataBindings', false);
+            canData.set.call(el, 'viewModel', importPromise);
+            canData.set.call(el, 'scope', importPromise);
+        } else {
+            var frag = tagData.subtemplate ? tagData.subtemplate(scope, tagData.options) : document.createDocumentFragment();
+            var nodeList = nodeLists.register([], undefined, true);
+            events.one.call(el, 'removed', function () {
+                nodeLists.unregister(nodeList);
+            });
+            el.appendChild(frag);
+            nodeLists.update(nodeList, el.childNodes);
+        }
+    });
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
