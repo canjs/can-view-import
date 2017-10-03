1be3244c7b8c78484d9ad4c899b5de0913ccc66e
Juan Orozco <hi@juanorozco.com>
2017-10-03 11:41:30 -0400

    3.2.1

---
+++ dist/amd/can-view-import.js
@@ -0,0 +1,72 @@
+/*can-view-import@3.2.0#can-view-import*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-util/js/assign',
+    'can-util/dom/data',
+    'can-util/dom/document',
+    'can-util/dom/child-nodes',
+    'can-util/js/import',
+    'can-util/dom/mutate',
+    'can-view-nodelist',
+    'can-view-callbacks',
+    'can-event',
+    'can-util/js/log',
+    'can-util/js/dev'
+], function (require, exports, module) {
+    var assign = require('can-util/js/assign');
+    var canData = require('can-util/dom/data');
+    var DOCUMENT = require('can-util/dom/document');
+    var getChildNodes = require('can-util/dom/child-nodes');
+    var importer = require('can-util/js/import');
+    var mutate = require('can-util/dom/mutate');
+    var nodeLists = require('can-view-nodelist');
+    var viewCallbacks = require('can-view-callbacks');
+    var tag = viewCallbacks.tag;
+    var events = require('can-event');
+    var canLog = require('can-util/js/log');
+    var dev = require('can-util/js/dev');
+    function processImport(el, tagData) {
+        var moduleName = el.getAttribute('from');
+        var templateModule = tagData.options.get('helpers.module');
+        var parentName = templateModule ? templateModule.id : undefined;
+        if (!moduleName) {
+            return Promise.reject('No module name provided');
+        }
+        var importPromise = importer(moduleName, parentName);
+        importPromise.catch(function (err) {
+            canLog.error(err);
+        });
+        canData.set.call(el, 'viewModel', importPromise);
+        canData.set.call(el, 'scope', importPromise);
+        var scope = tagData.scope.add(importPromise);
+        var handOffTag = el.getAttribute('can-tag');
+        if (handOffTag) {
+            var callback = tag(handOffTag);
+            if (!callback || callback === viewCallbacks.defaultCallback) {
+            } else {
+                canData.set.call(el, 'preventDataBindings', true);
+                callback(el, assign(tagData, { scope: scope }));
+                canData.set.call(el, 'preventDataBindings', false);
+                canData.set.call(el, 'viewModel', importPromise);
+                canData.set.call(el, 'scope', importPromise);
+            }
+        } else {
+            var frag = tagData.subtemplate ? tagData.subtemplate(scope, tagData.options) : DOCUMENT().createDocumentFragment();
+            var nodeList = nodeLists.register([], undefined, tagData.parentNodeList || true);
+            nodeList.expression = '<' + this.tagName + '>';
+            events.one.call(el, 'removed', function () {
+                nodeLists.unregister(nodeList);
+            });
+            mutate.appendChild.call(el, frag);
+            nodeLists.update(nodeList, getChildNodes(el));
+        }
+    }
+    [
+        'can-import',
+        'can-dynamic-import'
+    ].forEach(function (tagName) {
+        tag(tagName, processImport.bind({ tagName: tagName }));
+    });
+});
