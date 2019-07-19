e1be3d01921551a070f046debb145bc2a1b0f89f
Nils Lundquist <nils@bitovi.com>
2019-07-19 14:08:22 -0600

    don't use helpers object

--- can-view-import.js
+++ can-view-import.js
@@ -36,7 +36,7 @@ function processImport(el, tagData) {
 
 	// if this template was loaded by steal-stache, the module imported by this tag should already be loaded
 	// we can provide it immediately by adding the `export` property to the tag viewModel
-	var tagImportMap = tagData.scope.get("scope.helpers.tagImportMap");
+	var tagImportMap = tagData.scope.get("tagImportMap");
 	if (tagImportMap && tagImportMap[moduleName]) {
 		importPromise.export = tagImportMap[moduleName];
 	}
